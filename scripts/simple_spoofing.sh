6603327c70733f1b4c005a806428b69d9385236a
Aldo <98069346+aldoalfaro98@users.noreply.github.com>
2022-03-15 13:26:05 -0700

    Add files via upload

--- scripts/simple_spoofing.sh
+++ scripts/simple_spoofing.sh
@@ -3,7 +3,7 @@
 seed=123456789
 ticker=ABM
 date=20200101
-
+book_freq=30T
 log=simple_spoofing_${seed}
 
 # (1) Sparse Mean Reverting Oracle Fundamental
@@ -11,4 +11,8 @@ log=simple_spoofing_${seed}
 
 # (2) External Fundamental file
 fundamental_path=${PWD}/data/synthetic_fundamental.bz2
-python -u abides.py -c market_manipulation -t ${ticker} -d ${date} -f ${fundamental_path} -l ${log} -s ${seed} -e
+python -u abides.py -c market_manipulation -b${book_freq} -t ${ticker} -d ${date} -f ${fundamental_path} -l ${log} -s ${seed} -e
+
+#Plotting
+cd util/plotting && python -u liquidity_telemetry.py ../../log/simple_spoofing_123456789/EXCHANGE_AGENT.bz2 ../../log/simple_spoofing_123456789/ORDERBOOK_ABM_FULL.bz2 \
+-o simple_spoofing.png -c configs/plot_09.30_11.30.json && cd ../../
